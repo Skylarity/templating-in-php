99ef5c81f80d3e2f3a0c7be25296261e4a5df4ce
Skyler Rexroad <skyrex1095@gmail.com>
2016-08-18 08:28:59 -0600

    Created CATNIP

---
+++ catnip/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- My Custom CSS rlewis37@cnm.edu -->
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- jQuery (required for Bootstap's JS plugins) -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+        <title>Basic Bootstrap Wireframe Exercise</title>
+    </head>
+    <body class="sfooter">
+        <div class="sfooter-content">
+
+            <!--begin header and navbar-->
+            <header>
+                <div class="container">
+                    <!--Begin the Bootstrap Navbar - see documentation: http://getbootstrap.com/components/#navbar -->
+                    <nav class="navbar navbar-inverse">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="../">Bootstrap Wireframe Challenge!</a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Catnip</a></li>
+                                <li><a href="#">Toys</a></li>
+                                <li><a href="#">Kitty Treats</a></li>
+                                <li><a href="https://senator-arlo.bowtied.io/" target="_blank">Feel the Fuzzy</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </nav>
+                </div>
+            </header>
+
+            <!--begin main page content-->
+            <main>
+                <div class="container">
+
+                    <!--begin welcome row-->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="jumbotron">
+                                <h1>Meow! =^. .^=</h1>
+                                <p>CAAAAATNIIIIIIIIP!</p>
+                                <small>(An accurate representation of Arlo when there's catnip nearby)</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--begin 3 column row-->
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="content-box">
+                                <h3>Templating is fun!</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ab magnam, ea. Labore, architecto deserunt sed, incidunt explicabo eos rerum. In similique fuga ipsum ex, repellat, inventore expedita ea, iusto consequatur explicabo, provident ab. Temporibus, aut aliquam distinctio, eum suscipit sequi commodi molestias assumenda nobis? Ducimus velit, minus quibusdam reiciendis tempore corrupti in at perspiciatis minima, cumque maxime repellat cum soluta maiores quae sint non harum. Quas quisquam recusandae qui error libero accusamus ipsum consequatur, alias amet eaque repellat. Aspernatur facilis culpa, dolore nesciunt velit accusantium, ipsa dignissimos minima quod delectus ducimus laboriosam doloribus ex libero autem sit dolores vel sequi consequatur. Dignissimos sunt, excepturi, libero soluta inventore omnis at? Inventore temporibus ducimus voluptatibus accusantium, laboriosam. Eos, fuga nihil dolore ab, dolorem laudantium laborum voluptates ad vero dolor minus sed quisquam quam ipsa quaerat, repudiandae fugit doloremque unde deleniti. Explicabo sit, ut aperiam dolorum incidunt voluptates voluptatem et reiciendis numquam.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="content-box">
+                                <h3>Catnip here!</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio ab, pariatur odit labore. Labore sequi, itaque praesentium eaque aspernatur cupiditate dicta suscipit deleniti dolorum similique nobis, repudiandae fugit sunt, ipsum expedita. Aliquam, iure obcaecati voluptate quibusdam labore vitae, similique commodi aliquid perspiciatis provident aspernatur optio maxime magni reiciendis, inventore perferendis. Nostrum, et natus sunt modi quam commodi inventore! Maiores facere harum blanditiis, consectetur rerum iste libero possimus, eos quis necessitatibus provident praesentium! Eos, saepe delectus optio laudantium cumque, quos at? Aliquid, officiis, earum. Ipsum nihil aut quae repudiandae repellat laborum ex esse soluta quos facilis, ad alias, laboriosam est dolorem, illo, distinctio magni. Recusandae, praesentium harum odio assumenda maiores maxime delectus nemo, esse vel quam reprehenderit voluptates, molestias autem enim, modi doloribus exercitationem expedita quo. Molestias labore officiis voluptatibus porro quas rem quo illo excepturi iure beatae dolore esse saepe necessitatibus ducimus incidunt numquam, illum cum quasi. Reprehenderit, nulla, rerum.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="content-box">
+                                <h3>Catnip over there!</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, aspernatur, similique a ad quos velit mollitia consequuntur explicabo obcaecati inventore quam tempore, est numquam cupiditate sed officia quia consectetur repudiandae voluptatibus! Deleniti dicta magnam molestias, architecto, quam tempora quasi totam libero, quis quo aspernatur iure illum minima aut rerum, quod eius nemo nam quidem hic? Error rerum, at, alias nostrum obcaecati id quas mollitia tempora accusantium consequuntur suscipit, iste autem incidunt. Excepturi nam aliquid repudiandae sint quasi, dolorum modi libero accusamus veritatis! Autem doloribus consequatur tempora accusamus odit eaque provident, a repellat nobis adipisci dolorem et mollitia magni exercitationem alias, reprehenderit deleniti consequuntur tenetur, commodi aut doloremque voluptatem maiores quo maxime! Ex vitae, molestias quibusdam sequi. Sint iusto illo atque beatae voluptate harum magni ullam. Non odio neque facere quae et soluta quis enim labore consequatur, nulla rem nesciunt rerum. Commodi nemo, ullam iure sapiente dolorum nihil iusto non, quasi.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+        </div><!--.sfooter-content-->
+
+        <!--begin footer content-->
+        <footer>
+            <div class="container">
+                &copy; 2016 Back Paws for Bernie Campaign
+            </div>
+        </footer>
+    </body>
+</html>
